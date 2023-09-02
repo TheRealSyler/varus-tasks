c34b2550745c62efc6972bd5e8e2b44c34a036b4
Leonard Grosoli <hileo77@gmail.com>
2023-09-02 23:18:32 +0200

    copy env

--- install.sh
+++ install.sh
@@ -16,6 +16,8 @@ mkdir "$VARUS_INSTALL_PATH/varus-tasks/"
 cp -r build/* "$VARUS_INSTALL_PATH/varus-tasks/"
 echo "copied \"build/*\" to \"$VARUS_INSTALL_PATH/varus-tasks/\""
 
+cp .env "$VARUS_INSTALL_PATH/varus-tasks/.env"
+echo "copied \".env\" to \"$VARUS_INSTALL_PATH/varus-tasks/.env\""
 
-echo "node \"$(cygpath.exe -m "$VARUS_INSTALL_PATH/varus-tasks/index.js")\"" > "$VARUS_INSTALL_PATH/start-varus-tasks.bat"
+printf "cd varus-tasks\nstart \"Varus Tasks\" \"node\" \"$(cygpath.exe -m "$VARUS_INSTALL_PATH/varus-tasks/index.js")\"" > "$VARUS_INSTALL_PATH/start-varus-tasks.bat"
 echo "created \"$VARUS_INSTALL_PATH/start-varus-tasks.bat\""
